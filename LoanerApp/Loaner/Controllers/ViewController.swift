c4cc7dd3049cc6bba7dd415fe29a69afe3a6c2cc
Jess <jtrinh619@gmail.com>
2020-06-14 18:22:56 -0700

    saving items, app builds

--- LoanerApp/Loaner/Controllers/ViewController.swift
+++ LoanerApp/Loaner/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
@@ -15,6 +16,13 @@ class ViewController: UIViewController {
     var items: [Item] = []
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+
+        // Save the new items in the Managed Object Context
+        store.saveContext()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +38,8 @@ class ViewController: UIViewController {
     }
     
     func createNewItem() -> Item {
-        return Item(itemTitle: "Untitled Item")
+       let newItem = NSEntityDescription.insertNewObject(forEntityName: "Item", into: store.persistentContainer.viewContext) as! Item
+       return newItem
     }
     
     func add(saved item: Item) {
